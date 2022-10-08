16670fdfe80588aaa7d5a722dc3fb727859191b4
Jakob Bennemann <jakob@raidboxes.de>
2022-10-08 22:26:44 +0200

    fix test

--- tests/Http/Controllers/RegisterControllerTest.php
+++ tests/Http/Controllers/RegisterControllerTest.php
@@ -4,7 +4,7 @@ use Illuminate\Testing\TestResponse;
 
 it('generates options to create a new key', function () {
     /** @var TestResponse $response */
-    $response = $this->getJson(route('webauthn.register').'?'.http_build_query([
+    $response = $this->getJson(route('webauthn.register.get').'?'.http_build_query([
         'name' => 'Test',
         'display_name' => 'Test Display',
     ]));
